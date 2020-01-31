cff6174155287e8741309e377ad0b03c46daadf1
Unknown <bjesusga@gmail.com>
2020-01-31 13:29:43 +0100

    Estructurar los datos de las consultas

--- Aeronet/src/modelo/consultasBBDD.py
+++ Aeronet/src/modelo/consultasBBDD.py
@@ -4,20 +4,61 @@ Created on 29 ene. 2020
 @author: Jesus Brezmes Gil-Albarellos
 '''
 
-
 import src.modelo.globales as g
+import src.modelo.phStationObject as objecto
 
-class consultaBBDD():
+#Clase para realizar las diferentes consultas a la BBDD
+class consultaBBDD():  
     
+    #Metodo para consultar los distinct ph y station
     def getPhStation(self, cursor):
         listPhStation=[]
         try:
-            cursor.execute(g.sql2)
+            cursor.execute(g.sql1)
             data = cursor.fetchall()
             for row in data:
                 listPhStation.append(row)
         except ValueError:
             print(g.err1)
         
-        print (listPhStation)
         return data
+    
+    #Metodo que consulta a la BBDD por los datos completos de cada ph y station (dates y eprom type/subtype)
+    def getPhStationDates(self, cursor):
+        #listPhStation=[]
+        indices =[]
+        datosCompletos=[]
+        contador=0
+        try:
+            cursor.execute(g.sql2)
+            data = cursor.fetchall()
+            for row in data:
+                fechas = []
+                aux = str(row[0])+"  "+ str(row[1])
+                #listPhStation.append(row)
+                if aux not in indices:
+                    indices.append(aux)
+                    o = objecto.phStationObject()
+                    o.setPhStation(aux)
+                    o.setEType(row[4])
+                    o.setESubType(row[5])
+                    fechas.append(row[2])
+                    fechas.append(row[3])
+                    o.setDateOfUse(fechas)
+                    datosCompletos.append(o)
+                    contador+=1
+                else:
+                    o=datosCompletos[contador-1]
+                    datosCompletos.remove(o)
+                    fechas.append(row[2])
+                    fechas.append(row[3])
+                    o.setDateOfUse(fechas)
+                    datosCompletos.append(o)
+                    
+        except ValueError:
+            print(g.err1)
+            
+        print (datosCompletos)    
+        return data
+    
+    
